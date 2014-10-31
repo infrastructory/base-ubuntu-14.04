fb39caaa07b07315d45e8a60d5fddfa0293e4888
J Lynn <j@jaesharp.com>
2014-10-31 11:41:14 -0700

    our downstream images use chef to provision, preinstall it and dependency management to save time downstream

--- image/build_script/cleanup.sh
+++ image/build_script/cleanup.sh
@@ -2,6 +2,10 @@
 source /build/config/build
 set -x
 
+# Remove items used for building, since they aren't needed anymore
+apt-get -y purge build-essential autoconf
+apt-get -y autoremove --purge
+
 apt-get clean
 rm -rf /build
 rm -rf /tmp/* /var/tmp/*
